e6dc6105ea86eb28903645209fdfa166bdc1b99a
ahnafmottaki <ahnafmottaki2022@gmail.com>
2025-12-25 20:02:28 +0600

    practice array methods

--- array-methods.js
+++ array-methods.js
@@ -1,12 +1,23 @@
-let arr = ["I", "go", "home"];
-// * deleting an array element with the delete keyword but the length stays the same
-console.log(delete arr[0]);
-console.log(arr.length);
-
-// * with splice the element will be removed and also the length will be new
-console.log(arr.splice(0, 1, "ahnaf", "mottaki"));
-console.log(arr, arr.length);
-
-// * negative indexes are allowed
-console.log(arr.splice(-1, 0, "let's", "dance"));
-console.log(arr, arr.length);
+const fruits = [
+  "apple",
+  "banana",
+  "orange",
+  "grape",
+  "kiwi",
+  "banana",
+  "mango",
+];
+const numbers = [5, 2, 8, 1, 9, 3, 7, 4, 6];
+const mixed = [1, "apple", true, { name: "John" }, [2, 3], null, undefined];
+// Challenge 1: Use .at() to get the 3rd element from fruits array
+console.log(fruits.at(2));
+// Challenge 2: Use bracket notation [] to get the last element
+console.log(fruits[fruits.length - 1]);
+// Challenge 3: Use .at() with negative index to get the 2nd last element
+console.log(fruits.at(-2));
+// Challenge 4: Get the length of fruits array
+console.log(fruits.length);
+// Challenge 5: Check if numbers array contains at least 5 elements
+console.log(numbers.length >= 5);
+// Challenge 6: Check if mixed array is empty
+console.log(mixed.length === 0);
